ed071a4f316ea3ec8f7dce632ae63164f8e57ef6
Kylebriandd <21kbriand.acc@lwetb.ie>
2025-09-09 13:47:15 +0100

    Delete Q6.py

--- Q6.py
+++
@@ -1,8 +0,0 @@
-
-#author: Kyle Briand
-#date: 01/09/2025
-
-user_age = input("Input your age: ")
-user_age = int(user_age)
-user_age_next_year = user_age + 1
-print("You will be",user_age_next_year,"next year.")

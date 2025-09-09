f0cef60c64c1c08f67a6ff1e1924af05c4b3e9de
Kylebriandd <21kbriand.acc@lwetb.ie>
2025-09-09 13:50:04 +0100

    Add files via upload

---
+++ Pg 174/Q6.py
@@ -0,0 +1,11 @@
+
+#Kyle briand
+#02/09/2025
+
+#Write a python program that accepts the radius of a circle and prints its area.
+
+radius = input("Input the radius: ")
+radius = float(radius)
+
+area = radius * 3.141593654
+print("The area is",area,)
